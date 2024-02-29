14f6892781e6c97617cf33b514188355bd94edc3
season shrestha <np03a170120@heraldcollege.edu.np>
2024-02-29 20:22:54 +0545

    fix: product list filtered

--- src/app/Product/DashboardProducts.jsx
+++ src/app/Product/DashboardProducts.jsx
@@ -9,30 +9,28 @@ export default function DashboardProducts(product) {
 
   return (
     <>
-      {loginDetail?._id !== productDetail?.userId && (
-        <Link
-          to={`/product/detail/${productDetail?.userId}/${productDetail?._id}`}
-        >
-          <Card className="w-full h-full cursor-pointer hover:shadow-lg transition ease-in-out ">
-            <div className="gap-3 h-full">
-              <Image
-                className={"aspect-video overflow-hidden  object-cover "}
-                alt={productDetail?.name}
-                src={productDetail?.productImages[0]?.url}
-              />
-              <div className="text-left flex flex-col gap-1 leading-none px-4 py-6">
-                <h3 className="text-sm font-medium leading-none">
-                  {productDetail?.name}
-                </h3>
-                <p className="text-xs text-muted-foreground dark:text-muted-background">
-                  Available Quantity: {productDetail?.quantity}
-                </p>
-                <p className="text-sm font-medium">Rs.{productDetail?.price}</p>
-              </div>
+      <Link
+        to={`/product/detail/${productDetail?.userId}/${productDetail?._id}`}
+      >
+        <Card className="w-full h-full cursor-pointer hover:shadow-lg transition ease-in-out ">
+          <div className="gap-3 h-full">
+            <Image
+              className={"aspect-video overflow-hidden  object-cover "}
+              alt={productDetail?.name}
+              src={productDetail?.productImages[0]?.url}
+            />
+            <div className="text-left flex flex-col gap-1 leading-none px-4 py-6">
+              <h3 className="text-sm font-medium leading-none">
+                {productDetail?.name}
+              </h3>
+              <p className="text-xs text-muted-foreground dark:text-muted-background">
+                Available Quantity: {productDetail?.quantity}
+              </p>
+              <p className="text-sm font-medium">Rs.{productDetail?.price}</p>
             </div>
-          </Card>
-        </Link>
-      )}
+          </div>
+        </Card>
+      </Link>
     </>
   );
 }
